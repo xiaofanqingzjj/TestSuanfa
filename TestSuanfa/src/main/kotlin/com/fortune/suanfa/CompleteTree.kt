457b360de487284fc11c182394e4a9542dadf550
fortunexiao <fortunexiao@tencent.com>
2020-07-17 17:44:36 +0800

    1.一些树相关的算法例子

--- TestSuanfa/src/main/kotlin/com/fortune/suanfa/CompleteTree.kt
+++ TestSuanfa/src/main/kotlin/com/fortune/suanfa/CompleteTree.kt
@@ -8,32 +8,74 @@ package com.fortune.suanfa
 
 
 /**
- * 前序便利二叉树
+ * 遍历完全二叉树
+ *
+ * @param tree 一颗完全二叉树
+ * @param treeNode 使用节点在列表中的位置来表示树节点，注意是从1开始，
+ * @param traverseMethod 遍历方式1:前序遍历，2:中序遍历，3:后续遍历
+ * @param visit 遍历方式
  *
  */
-fun preTravarseTree(tree: IntArray, nodeIndex: Int, visit:(value: Int)->Unit) {
+fun <T> traverseTree(tree: Array<T>, treeNode: Int, traverseMethod: Int,  visit:(node: T)->Unit) {
+
 
     val nodeCount = tree.size
 
-    // leftChild
-    if (nodeIndex * 2 <= nodeCount) {
-        preTravarseTree(tree, nodeIndex * 2, visit)
-    }
+    when (traverseMethod) {
+        1-> {
+            visit(tree[treeNode - 1])
 
-    visit(tree[nodeIndex - 1])
+            // leftChild
+            if (treeNode * 2 <= nodeCount) {
+                traverseTree(tree, treeNode * 2, traverseMethod, visit)
+            }
 
-    // rightChild
-    if (nodeIndex * 2  + 1 <= nodeCount) {
-        preTravarseTree(tree, nodeIndex  * 2 + 1, visit)
+
+            // rightChild
+            if (treeNode * 2  + 1 <= nodeCount) {
+                traverseTree(tree, treeNode  * 2 + 1, traverseMethod, visit)
+            }
+        }
+        2-> {
+
+
+            // leftChild
+            if (treeNode * 2 <= nodeCount) {
+                traverseTree(tree, treeNode * 2, traverseMethod, visit)
+            }
+
+            visit(tree[treeNode - 1])
+
+
+            // rightChild
+            if (treeNode * 2  + 1 <= nodeCount) {
+                traverseTree(tree, treeNode  * 2 + 1, traverseMethod, visit)
+            }
+        }
+        3-> {
+
+            // leftChild
+            if (treeNode * 2 <= nodeCount) {
+                traverseTree(tree, treeNode * 2, traverseMethod, visit)
+            }
+
+
+            // rightChild
+            if (treeNode * 2  + 1 <= nodeCount) {
+                traverseTree(tree, treeNode  * 2 + 1, traverseMethod, visit)
+            }
+
+            visit(tree[treeNode - 1])
+        }
     }
 
 }
 
 fun main(args: Array<String>) {
 
-    val tree = intArrayOf(1, 2, 3, 4, 5 , 6)
+    val tree: Array<Int> = arrayOf(1, 2, 3, 4, 5 , 6)
 
-    preTravarseTree(tree, 1) {
+    traverseTree(tree, 1, 3) {
         println("node:$it")
     }
 
---
+++ TestSuanfa/src/main/kotlin/com/fortune/suanfa/Sort.kt
@@ -0,0 +1,15 @@
+package com.fortune.suanfa
+
+
+/**
+ * 树选择排序
+ *
+ */
+fun treeSelectSort(list: IntArray) {
+
+
+
+    //
+    val tree = IntArray(list.size * 2 -1 )
+
+}
