7e59038937a9c8c6d32581038c4c931daad19265
xiaofanqing <xiaofanqingzjj@163.com>
2020-07-28 23:26:26 +0800

    1.增加二叉树的一些算法

--- TestSuanfa/src/main/kotlin/com/fortune/suanfa/tree/BinTreeNode.kt
+++ TestSuanfa/src/main/kotlin/com/fortune/suanfa/tree/BinTreeNode.kt
@@ -9,6 +9,37 @@ class BinTreeNode<T> (var data: T,
                       var left: BinTreeNode<T>? = null,
                       var right: BinTreeNode<T>? = null) {
 
+    companion object {
+
+        // 一些工具方法
+
+        /**
+         * 从列表中创建一颗排序二叉排序树
+         * 主要用来测试用
+         */
+        fun fromList(list: IntArray): BinTreeNode<Int>? {
+            var tree : BinTreeNode<Int>? = null
+            list.forEach {
+                tree = insertToBST(tree, it)
+            }
+            return tree
+        }
+
+        private fun insertToBST(tree: BinTreeNode<Int>?, value: Int): BinTreeNode<Int> {
+            if (tree == null) {
+                return BinTreeNode(value)
+            }
+            when {
+                tree.data < value -> tree.right = insertToBST(tree.right, value) // 插入右孩子
+                tree.data > value -> tree.left = insertToBST(tree.left, value) // 插入左孩子
+                else -> { // 命中，不插入
+
+                }
+            }
+            return tree
+        }
+    }
+
 
     /**
      * 遍历二叉树
@@ -35,6 +66,56 @@ class BinTreeNode<T> (var data: T,
         }
     }
 
+    /**
+     * 返回二叉树的节点数量
+     */
+    fun nodeCount(): Int {
+        return nodeCount(this)
+    }
+
+    /**
+     * 递归计算二叉树节点的个数
+     */
+    private fun nodeCount(node: BinTreeNode<T>?): Int {
+        if (node == null) return 0
+        return nodeCount(node.left) + nodeCount(node.right) + 1
+    }
+
+    /**
+     * 返回二叉树的深度
+     */
+    fun depth(): Int {
+        return depth(this)
+    }
+
+    /**
+     * 返回二叉树的深度
+     */
+    private fun depth(tree: BinTreeNode<T>?): Int {
+        if (tree == null) return 0
+        val leftDepth = depth(tree.left)
+        val rightDepth = depth(tree.right)
+        return if (leftDepth > rightDepth) leftDepth + 1 else rightDepth + 1
+    }
+
+    fun reverse(){
+        reverse(this)
+    }
+
+    fun reverse(tree: BinTreeNode<T>?) {
+        if (tree == null) return
+
+        reverse(tree.left)
+        reverse(tree.right)
+
+        val tmp = tree.left
+        tree.left = tree.right
+        tree.right = tmp
+
+
+    }
+
+
     fun reOrder(visit: (node: BinTreeNode<T>)->Unit) {
         visit(this)
         left?.reOrder(visit)
@@ -51,4 +132,23 @@ class BinTreeNode<T> (var data: T,
     override fun toString(): String {
         return super.toString()
     }
+}
+
+fun main(args: Array<String>) {
+    val list = intArrayOf(2, 1, 5, 3, 4, 8)
+    val tree = BinTreeNode.fromList(list)
+
+    tree?.traversel(1) {
+        println("node:${it.data}")
+    }
+
+    println("tree node count:${tree?.nodeCount()}")
+    println("tree depth :${tree?.depth()}")
+
+    tree?.reverse()
+    tree?.traversel(0) {
+        println("node:${it.data}")
+    }
+
+
 }
---
+++ TestSuanfa/src/main/kotlin/com/fortune/suanfa/tree/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *
+ * 提供数据结构树相关的例子和算法
+ *
+ * @author xiaofanqing
+ */
+package com.fortune.suanfa.tree;
